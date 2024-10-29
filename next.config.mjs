c6cdb03f3c057c5a0bdbc65b0b4f6598502619a9
Maxime Dénervaud <maxime.denervaud@students.hevs.ch>
2024-10-29 07:14:35 +0100

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "website-back-fincargo.onrender.com",
+        hostname: "fincargo-backend-website.onrender.com",
         pathname: "/uploads/**",
       },
     ],
