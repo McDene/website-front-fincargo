c1dc21524a8866309af1a3411ab5e17599a4279b
Maxime Dénervaud <maxime.denervaud@students.hevs.ch>
2024-10-29 09:01:31 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "*",
+            value: "https://website-front-fincargo.vercel.app",
           },
           {
             key: "Access-Control-Allow-Methods",
