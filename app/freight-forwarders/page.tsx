29de532b4460edb4ebf3da0c3e045b897d2d4339
Maxime Dénervaud <maxime.denervaud@students.hevs.ch>
2024-11-06 18:23:53 +0100

    Bug fix

--- app/freight-forwarders/page.tsx
+++ app/freight-forwarders/page.tsx
@@ -5,6 +5,8 @@ import ClipLoader from "react-spinners/ClipLoader";
 import Header from "@/components/Header/Main";
 import HeroFF from "@/components/Hero/HeroFF";
 import FeatureFF from "@/components/Feature/FeatureFF";
+// import BenefitFF from "@/components/Benefit/BenefitFF";
+import InviteFF from "@/components/Invite/InviteFF";
 import Footer from "@/components/Footer";
 import { fetchAPI } from "@/lib/utils";
 import Grid from "@/components/Gride/Gride";
@@ -13,7 +15,7 @@ export default function FreightForwardersPage() {
   const [heroData, setHeroData] = useState(null);
   const [featureData, setFeatureData] = useState(null);
   // const [benefitData, setBenefitData] = useState(null);
-  // const [inviteData, setInviteData] = useState(null);
+  const [inviteData, setInviteData] = useState(null);
   // const [faqData, setFaqData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [showLoader, setShowLoader] = useState(false);
@@ -29,7 +31,7 @@ export default function FreightForwardersPage() {
           heroResponse,
           featureResponse,
           // benefitResponse,
-          // inviteResponse,
+          inviteResponse,
           // faqResponse,
         ] = await Promise.all([
           fetchAPI(
@@ -41,18 +43,18 @@ export default function FreightForwardersPage() {
           // fetchAPI(
           //   "/api/benefits?filters[Page][$eq]=FreightForwarders&populate[Benefit][populate][Card][populate]=Image"
           // ),
+          fetchAPI(
+            "/api/invites?filters[Page][$eq]=FreightForwarders&populate=Image"
+          ),
           // fetchAPI(
-          //   "/api/invites?filters[Page][$eq]=FreightForwarders&populate=Image"
-          // ),
-          // fetchAPI(
-          //   "/api/faqs?filters[Page][$eq]=FreightForwarders&populate[FAQ][populate]=Accordion"
+          //   "/api/faqs?filters[Page][$eq]=FreightForwarder&populate[FAQ][populate]=Accordion"
           // ),
         ]);
 
         setHeroData(heroResponse?.data?.[0]?.Hero || null);
         setFeatureData(featureResponse?.data?.[0] || null);
         // setBenefitData(benefitResponse?.data?.[0] || null);
-        // setInviteData(inviteResponse?.data?.[0] || null);
+        setInviteData(inviteResponse?.data?.[0] || null);
         // setFaqData(faqResponse?.data?.[0] || null);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -82,11 +84,11 @@ export default function FreightForwardersPage() {
         {heroData && <HeroFF heroData={heroData} />}
         {featureData && <FeatureFF featureData={featureData} />}
         <Grid />
-        {/*
-        {benefitData && <BenefitFF benefitData={benefitData} />}
+
+        {/* {benefitData && <BenefitFF benefitData={benefitData} />} */}
         {inviteData && <InviteFF inviteData={inviteData} />}
-        {faqData && <FaqFF faqData={faqData} />} */}
-        <Grid />
+        {/* {faqData && <FaqFF faqData={faqData} />}  */}
+
         <Footer />
       </>
     )
